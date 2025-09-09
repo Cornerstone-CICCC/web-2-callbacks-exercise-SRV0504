b243308cd9a796a38724f880763ead51193615b6
Samuel Rios <srios050402@gmail.com>
2025-09-08 17:37:29 -0700

    complete

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,22 @@
 const fs = require("fs");
 
-fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
-})
+fs.readFile("./firstname.txt", "utf-8", (e, first) => {
+  if (e) return console.error(e);
+
+  fs.readFile("./lastname.txt", "utf-8", (e, last) => {
+    if (e) return console.error(e);
+
+    fs.readFile("./age.txt", "utf-8", (e, age) => {
+      if (e) return console.error(e);
+
+      fs.readFile("./hobbies.txt", "utf-8", (e, hobbiesData) => {
+        if (e) return console.error(e);
+
+        const hobbies = JSON.parse(hobbiesData);
+        console.log(
+          `${first.trim()} ${last.trim()} is ${age.trim()} years old and his hobbies are ${hobbies[0]} and ${hobbies[1]}`
+        );
+      });
+    });
+  });
+});
